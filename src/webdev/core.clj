178691f22b7cc3c5bdba0115781ada8ff68c9b14
Rahul Horé <hore.rahul@gmail.com>
2015-07-24 23:23:20 -0400

    Use wrap-reload middleware
    Allow code changes without manually restarting the server.
    Only times a manual restart will be needed is if we want to add a new library or
    if we want to change our main function again.
    
    1. Include ring middleware reload namespace (we only want wrap-reload)
    2. Add a -dev-main function that will only be called in development
    3. Create a dev profile in project.clj to ensure -dev-main runs during development

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,10 +1,11 @@
 (ns webdev.core
-  (:require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn greet [{:keys [uri] :as req}]
   (if (= uri "/")
     {:status 200
-     :body "Hello, World!"
+     :body "Hello, World! - sans reloading!"
      :headers {}}
     {:status 404
      :body "Not Found"
@@ -12,4 +13,8 @@
 
 (defn -main [port]
   (jetty/run-jetty greet
+                   {:port (Integer. port)}))
+
+(defn -dev-main [port]
+  (jetty/run-jetty (wrap-reload #'greet)
                    {:port (Integer. port)}))
