ef68d53e2f5a12532ed6925c42e4430ec7d049e9
Rahul Horé <hore.rahul@gmail.com>
2015-07-24 23:09:50 -0400

    Respond with 200 for root path only; 404 otherwise

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,10 +1,14 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]))
 
-(defn greet [req]
-  {:status 200
-   :body "Hello, World!"
-   :headers {}})
+(defn greet [{:keys [uri] :as req}]
+  (if (= uri "/")
+    {:status 200
+     :body "Hello, World!"
+     :headers {}}
+    {:status 404
+     :body "Not Found"
+     :headers {}}))
 
 (defn -main [port]
   (jetty/run-jetty greet
