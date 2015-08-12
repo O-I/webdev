da21f871843bb0ad0821805b1e920a540a956187
Rahul Horé <hore.rahul@gmail.com>
2015-08-11 21:59:48 -0400

    Add wrap-resource middleware
    Given a matching URI, serves the appropriate static assets bypassing
    the handler. If the URI does not match, it passes the URI on to the
    handler.

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -5,6 +5,7 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]
             [compojure.core :refer [defroutes ANY GET POST PUT DELETE]]
             [compojure.route :refer [not-found]]
             [ring.handler.dump :refer [handle-dump]]))
@@ -72,9 +73,11 @@
 
 (def app
   (wrap-server
-   (wrap-db
-    (wrap-params
-     routes))))
+   (wrap-resource
+    (wrap-db
+     (wrap-params
+      routes))
+    "static")))
 
 (defn -main [port]
   (items/create-table db)
