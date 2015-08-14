38e33f74d2151a4bff25cdaaa8a24a1fafa00d4f
Rahul Horé <hore.rahul@gmail.com>
2015-08-13 23:02:38 -0400

    Add items table

--- src/webdev/item/view.clj
+++ src/webdev/item/view.clj
@@ -11,6 +11,20 @@
           [:link {:href "/bootstrap/css/bootstrap.min.css"
                   :rel :stylesheet}]]
          [:body
-          [:div.container]
+          [:div.container
+           [:h1 "Items"]
+           [:div.row
+            (if (seq items)
+              [:table.table.table-striped
+               [:thead
+                [:tr
+                 [:th "Name"]
+                 [:th "Description"]]]
+               [:tbody
+                (for [i items]
+                  [:tr
+                   [:th (:name i)]
+                   [:th (:description i)]])]]
+              [:div.col-sm-offset-1 "There are no new items."])]]
           [:script {:src "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"}]
           [:script {:src "/bootstrap/js/bootstrap.min.js"}]]))
