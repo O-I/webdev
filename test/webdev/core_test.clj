fdb78d263d69c825caca78ecfb767a59cb7d0d7b
Rahul Horé <hore.rahul@gmail.com>
2015-08-23 22:22:07 -0400

    Begin testing routes

--- test/webdev/core_test.clj
+++ test/webdev/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [webdev.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn request [resource test-app & params]
+  (test-app {:request-method :get :uri resource :params (first params)}))
+
+(deftest test-routes
+  (is (= 404 (:status (request "/path-to-nowhere" app))))
+  (is (= "Page not found." (:body (request "/path-to-nowhere" app))))
+  (is (= 200 (:status (request "/" app))))
+  (is (= "Hello, World!" (:body (request "/" app)))))
