6df0f3a73dd67372d44466af2a9b7f99b5bedf02
Rahul Horé <hore.rahul@gmail.com>
2015-09-24 07:42:21 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject webdev "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "ljspt: a sample todo app in Clojure"
+  :url "https://ljspt.herokuapp.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-alpha5"]
                  [ring "1.4.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
