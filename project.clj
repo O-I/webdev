376596389148893bb89282b6e30bac5672a489b8
Rahul Horé <hore.rahul@gmail.com>
2015-09-27 23:02:11 -0400

    Add ClojureScript dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0-alpha5"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [ring "1.4.0"]
                  [compojure "1.4.0"]
                  [hiccup "1.0.5"]
